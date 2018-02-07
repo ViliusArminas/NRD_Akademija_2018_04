767aaf6503319610e11423bfa5fe6154e691ac18
Vilius Arminas <var@kn.nrd.lt>
2018-02-07 14:04:14 +0200

    Padaryti sarasai su testiniais duomenimis, duomenu modeliai, modalinis langas, font awesome

--- inventory-management/src/app/components/employees/employees.component.ts
+++ inventory-management/src/app/components/employees/employees.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { EmployeeModel } from '../../models/employees/employees.model';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
     selector: 'app-employees-component',
@@ -6,7 +8,70 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class EmployeesComponent implements OnInit {
-    constructor() { }
+
+    viewEmployeeIndex: any;
+
+    employees: EmployeeModel[] = [
+        {
+            id: 1,
+            firstName: 'Mindaugas',
+            inventory: [
+                {
+                    id: 1,
+                    name: 'Dell monitor',
+                },
+                {
+                    id: 2,
+                    name: 'Logitech mouse',
+                },
+                {
+                    id: 3,
+                    name: 'Dell computer',
+                },
+                {
+                    id: 7,
+                    name: 'Logitech keyboard',
+                },
+            ]
+        },
+        {
+            id: 1,
+            firstName: 'Kęstutis',
+            inventory: [
+                {
+                    id: 5,
+                    name: 'Asus monitor',
+                },
+                {
+                    id: 2,
+                    name: 'Logitech mouse',
+                },
+                {
+                    id: 3,
+                    name: 'Dell computer',
+                },
+            ]
+        },
+        {
+            id: 1,
+            firstName: 'Linas',
+            inventory: []
+        }
+    ];
+
+    employeeHeaders: any[] = [
+        { col: 'no', label: '#' },
+        { col: 'firstName', label: 'First Name' },
+        { col: 'inventorySize', label: 'Inventory Size' }
+    ];
+
+    constructor(private modalService: NgbModal) { }
 
     ngOnInit() { }
+
+    open(modal, eployeeIndex) {
+        this.modalService.open(modal);
+        this.viewEmployeeIndex = eployeeIndex;
+        console.log(this.viewEmployeeIndex);
+    }
 }
